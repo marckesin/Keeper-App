5e03dd273c23467467add18e20f284c7ec60a8bb
marckesin <192.168.2.1@protonmail.com>
2021-08-09 02:25:19 -0300

    meta description, fonts

---
+++ build/precache-manifest.e66b7de854d1a882d56c58f846794314.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e180eb206869fa7345a48e9c4bdeb4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2799d0e03cd6b4331d4d",
+    "url": "/static/js/2.ec8a0350.chunk.js"
+  },
+  {
+    "revision": "29d1b20f2cb1dba269a9",
+    "url": "/static/js/main.7f26c352.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
